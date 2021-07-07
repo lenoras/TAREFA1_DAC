b6bc1bed6f3be8a4613cfa26282321c171bb4dd1
lenora schwaitzer <lenoras@id.uff.br>
2021-07-07 09:30:04 -0700

    melhorando estilo

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -60,30 +60,13 @@ public class HelloServlet extends HttpServlet {
             out.println("<head><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">\n"
             		+ "    ");
             out.println("<title>Servlet HelloServlet</title>");            
-            out.println("</head>");
-            out.println("<body><br><nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\n"
-            		+ "  <a class=\"navbar-brand\" href=\".\\index.html\">ServletJava</a>\n"
-            		+ "  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n"
-            		+ "    <span class=\"navbar-toggler-icon\"></span>\n"
-            		+ "  </button>\n"
-            		+ "      <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n"
-            		+ "    <ul class=\"navbar-nav mr-auto\">\n"
-            		+ "      <li class=\"nav-item active\">\n"
-            		+ "        <a class=\"nav-link\" href=\".\\index.html\">Home <span class=\"sr-only\"></span></a>\n"
-            		+ "      </li>\n"
-            		+ "      <li class=\"nav-item dropdown\">\n"
-            		+ "        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n"
-            		+ "          Opções\n"
-            		+ "        </a>\n"
-            		+ "        <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n"
-            		+ "          <a class=\"dropdown-item\" href=\".\\alomundo?lang=pt&nome=Fulano\">Método GET</a>\n"
-            		+ "          <a class=\"dropdown-item\" href=\".\\helloform.html\">Método POST(esta página)</a>\n"
-            		+ "        </div>\n"
-            		+ "  </div>\n"
-            		+ "</nav><div class=\"container\"><div class=\"row\"><div class=\"col-md-9 ms-md-auto\"><br>");
+            out.println("</head><br><div class=\"container\"><div class=\"row\"><div class=\"col-md-9 ms-md-auto\"><br>");
             out.println("<h1><p class=\"text-danger\">Servlet HelloServlet</p></h1>");
             out.println("<p>" + msg + "</p>");
-            out.println("</div></div></div></body>");
+            out.println("</div></div></div><script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n"
+            		+ "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>\n"
+            		+ "    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\n"
+            		+ "    </body>");
             out.println("</html>");
         }
     }
@@ -121,30 +104,13 @@ public class HelloServlet extends HttpServlet {
             out.println("<head><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">\n"
             		+ "    ");
             out.println("<title>Servlet HelloServlet</title>");            
-            out.println("</head>");
-            out.println("<body><nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\n"
-            		+ "  <a class=\"navbar-brand\" href=\".\\index.html\">ServletJava</a>\n"
-            		+ "  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n"
-            		+ "    <span class=\"navbar-toggler-icon\"></span>\n"
-            		+ "  </button>\n"
-            		+ "      <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n"
-            		+ "    <ul class=\"navbar-nav mr-auto\">\n"
-            		+ "      <li class=\"nav-item active\">\n"
-            		+ "        <a class=\"nav-link\" href=\".\\index.html\">Home <span class=\"sr-only\"></span></a>\n"
-            		+ "      </li>\n"
-            		+ "      <li class=\"nav-item dropdown\">\n"
-            		+ "        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n"
-            		+ "          Opções\n"
-            		+ "        </a>\n"
-            		+ "        <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n"
-            		+ "          <a class=\"dropdown-item\" href=\".\\alomundo?lang=pt&nome=Fulano\">Método GET</a>\n"
-            		+ "          <a class=\"dropdown-item\" href=\".\\helloform.html\">Método POST(esta página)</a>\n"
-            		+ "        </div>\n"
-            		+ "  </div>\n"
-            		+ "</nav><br><div class=\"container\"><div class=\"row\"><div class=\"col-md-9 ms-md-auto\"><br>");           
+            out.println("</head><br><div class=\"container\"><div class=\"row\"><div class=\"col-md-9 ms-md-auto\"><br>");           
             out.println("<h1><p class=\"text-danger\">Servlet HelloServlet</p></h1>");
             out.println("<p>" + msg + "</p>");
-            out.println("</div></div></div></body>");
+            out.println("</div></div></div><script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n"
+            		+ "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>\n"
+            		+ "    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\n"
+            		+ "    </body>");
             out.println("</html>");
         }
     }
